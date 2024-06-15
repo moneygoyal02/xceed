97fb9576ff886da824e547d6b9eafac911ba8ad7
moneygoyal02 <goyalmoney95@gmail.com>
2024-06-15 22:41:40 +0530

    ..

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
 
-// app.use('/', require('./server/routes/admin'));
+app.use('/', require('./server/routes/admin'));
 app.use('/',require('./server/routes/main'));
 
 app.listen(PORT, () => {
